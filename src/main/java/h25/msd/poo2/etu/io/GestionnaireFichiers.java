3f0fcea7b989e492d51f5f156f5d03d51d5db401
2351817 <2351817@cegeplimoilou.ca>
2025-05-02 13:23:00 -0400

    Implementation de la classe GestionnaireFichiers

--- src/main/java/h25/msd/poo2/etu/io/GestionnaireFichiers.java
+++ src/main/java/h25/msd/poo2/etu/io/GestionnaireFichiers.java
@@ -1,14 +1,59 @@
 package h25.msd.poo2.etu.io;
 
+import h25.msd.poo2.echange.AlgorithmeI;
+import h25.msd.poo2.echange.Dossiers;
+import h25.msd.poo2.echange.GestionnaireFichierI;
+import h25.msd.poo2.etu.exception.TP3Exception;
+import h25.msd.poo2.etu.utilisateur.AbstractUtilisateur;
+
 import java.io.File;
 
 
-public class GestionnaireFichiers  {
+public class GestionnaireFichiers implements GestionnaireFichierI {
 
 
+    @Override
+    public void prepareDossiersRequis() {
 
+    }
 
+    @Override
+    public void viderDossiersFichiers() {
 
+    }
 
+    @Override
+    public void enregistreTexte(File fichier, String texte) throws TP3Exception {
 
+    }
+
+    @Override
+    public String chargeTexte(File fichier) throws TP3Exception {
+        return "";
+    }
+
+    @Override
+    public File getDossier(Dossiers dossier) {
+        return null;
+    }
+
+    @Override
+    public void sauvegarderParametreSelectionne(AlgorithmeI algo) throws TP3Exception {
+
+    }
+
+    @Override
+    public void chargeParametresDansAlgo(AlgorithmeI algoRecherche) throws TP3Exception {
+
+    }
+
+    @Override
+    public void sauvegardeUtilisateur(AbstractUtilisateur abstractUtilisateur) throws TP3Exception {
+
+    }
+
+    @Override
+    public AbstractUtilisateur chargeUtilisateur() throws TP3Exception {
+        return null;
+    }
 }
