6a5b0f2ac604de46ec23574e01d6c6a3b4a38e00
EDITH-RAND\edith <edithrand505@gmail.com>
2025-05-02 10:48:13 -0500

    Initialisation 2 projet Eddy

---
+++ src/main/java/h25/msd/poo2/etu/Model.java
@@ -0,0 +1,29 @@
+package h25.msd.poo2.etu;
+
+
+
+//**********************************************************
+//   Remplacer cette classe par la votre (celle du tp2)    *
+//**********************************************************
+
+
+import h25.msd.poo2.echange.ApplicationModelI;
+import h25.msd.poo2.echange.ApplicationUI;
+import h25.msd.poo2.etu.io.GestionnaireFichiers;
+
+public class Model implements ApplicationModelI {
+    private GestionnaireFichiers gestionnaireDossiers;
+
+
+    @Override
+    public void initialise(ApplicationUI ui) {
+
+    }
+
+    @Override
+    public String getNomsAuteurs() {
+        return "Vos noms";
+    }
+
+
+}
